bdc41fa2e1ae4e724b5c636492e7539690cd44a3
Pokey Rule <755842+pokey@users.noreply.github.com>
2022-11-22 14:53:48 +0000

    More cleanup

--- upgrade_knausj/setup.py
+++ upgrade_knausj/setup.py
@@ -1,33 +1,35 @@
-from re import M
-
 from git.repo import Repo
 
 from upgrade_knausj.util import error_and_exit
 
-# FIXME: Support http url
-knausj_uri = "git@github.com:knausj85/knausj_talon.git"
+# FIXME: Support ssh url
+knausj_uri = "https://github.com/knausj85/knausj_talon.git"
+mine_main_local_name = "mine_main"
+knausj_main_local_name = "knausj_main"
 
 
-def setup_mine(repo: Repo, my_repo_uri: str, my_branch: str):
+def setup_mine(repo: Repo, remote_uri: str, remote_branch_name: str):
     if "mine" not in repo.remotes:
-        mine = repo.create_remote("mine", my_repo_uri)
-        print(f"\nCreated remote '{mine.name}' for '{my_repo_uri}'")
+        mine = repo.create_remote("mine", remote_uri)
+        print(f"\nCreated remote '{mine.name}' for '{remote_uri}'")
     else:
         mine = repo.remotes.mine
 
     mine.fetch()
 
     try:
-        mine_remote_main = mine.refs[my_branch]
+        mine_remote_main = mine.refs[remote_branch_name]
     except IndexError:
         error_and_exit(
-            f"Branch '{my_branch}' does not exist on your repo\n"
+            f"Branch '{remote_branch_name}' does not exist on your repo\n"
             "Run with '--my-repo-uri' arg to specify your branch name (probably 'master')"
         )
 
-    if "mine_main" not in repo.heads:
-        mine_main = repo.create_head("mine_main", mine_remote_main)
-        print(f"Created branch '{mine_main.name}' to track '{mine.name}:{my_branch}'")
+    if mine_main_local_name not in repo.heads:
+        mine_main = repo.create_head(mine_main_local_name, mine_remote_main)
+        print(
+            f"Created branch '{mine_main.name}' to track '{mine.name}:{remote_branch_name}'"
+        )
         mine_main.set_tracking_branch(mine_remote_main)
     else:
         mine_main = repo.heads.mine_main
@@ -53,8 +55,8 @@ def setup_knausj(repo: Repo):
 
     knausj_remote_main = knausj.refs.main
 
-    if "knausj_main" not in repo.heads:
-        knausj_main = repo.create_head("knausj_main", knausj_remote_main)
+    if knausj_main_local_name not in repo.heads:
+        knausj_main = repo.create_head(knausj_main_local_name, knausj_remote_main)
         print(f"Created branch '{knausj_main.name}' to track '{knausj.name}:main'")
         knausj_main.set_tracking_branch(knausj_remote_main)
     else:
